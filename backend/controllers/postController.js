1781b9efd92a2abed32861a516bdd864c5141ef3
Mayankk2504 <mayankkapoormk1@gmail.com>
2024-11-21 20:53:57 +0530

    Create Post

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,7 +1,50 @@
+import Post from "../models/postModel.js";
+import User from "../models/userModel.js";
+import cloudinary from "cloudinary"
+import { message } from "../utils/message.js";
 import { Response } from "../utils/response.js"
 
 export const createPost = async (req, res) => {
     try {
+
+        //parsing body data
+        const{ image, caption, location} = req.body;
+
+        //Checking body data
+        if(!caption){
+            return Response(res, 400, false, message.missFieldsMessage);
+        }
+
+        //Check image
+        if(!image){
+            return Response(res, 400, false, message.imageMissingMessage);
+        }
+
+        const imageUpload = await cloudinary.v2.uploader.upload(image, {
+            folder: 'posts'
+        })
+
+        //Create Post
+        let post = await Post.Create({
+            image: {
+                public_id: imageUpload.public_id,
+                url: imageUpload.url
+            },
+            caption,
+            location
+        })
+
+        //Set owner details
+        post.owner = req.user_id;
+        await post.save();
+
+        // Set post in user
+        let user = await User.findById(req.user._id);
+        user.posts.unshift(post._id);
+        await user.save();
+
+        //send response
+        Response(res, 201, true, message.postCreatedMessage);
         
     } catch (error) {
         Response(res, 500, false, error.message);
--- backend/utils/message.js
+++ backend/utils/message.js
@@ -21,4 +21,6 @@ export const message = {
     userProfileFoundMessage: "User profile found",
     userProfileUpdatedMessage: "User profile updated successfully",
     userNotVerifiedMessage: "User not verified. Please verify your email",
+    imageMissingMessage: "please provide image",
+    postCreatedMessage: "Post created successfuly",
 }
